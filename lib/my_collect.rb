3217f5475beb2c8cd4619850927445a981def9c3
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-19 23:36:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,1 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(array)
-i = 0 
-name_collection = []
-while i < array.length 
-name_collection.push yield(array [i])
-i += 1 
- end 
- name_collection 
-end
-
 
