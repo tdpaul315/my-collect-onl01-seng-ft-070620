ce764e8c2a15ef5e2236f1e03f1ca8667288a81b
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-19 23:29:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,10 @@ my_collect(array)
 i = 0 
 name_collection = []
 while i < array.length 
-name_collection.push yield do |name|
-  name.split(" ").first
-end
+name_collection.push yield 
+(array [i])
 
+i =+ 1 
+ end 
+ name_collection 
+end
