d88b067513b80ddc537b4cce8c1b1915053cdff6
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-19 23:28:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-my_collect(array) do |name|
+my_collect(array)
+i = 0 
+name_collection = []
+while i < array.length 
+name_collection.push yield do |name|
   name.split(" ").first
 end
 
